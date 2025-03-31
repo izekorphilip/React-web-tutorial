b388229d03ef4530df6bca12e34cca89477e52cc
izekorphilip <izekorphilip5@gmail.com>
2025-03-31 10:34:11 +0100

    eslint install and committed

--- auth.ts
+++ auth.ts
@@ -1,11 +1,10 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcryptjs from 'bcryptjs';
 import postgres from 'postgres';
-
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
